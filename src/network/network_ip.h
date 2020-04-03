24120876247aeaa7c36e3e126bb7757970edc652
Yash J Shetty <yash17shetty@gmail.com>
2020-04-03 18:55:49 -0400

    ys - networking registration works

--- src/network/network_ip.h
+++ src/network/network_ip.h
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "../serialize/deserialize.h"
 
 class NodeInfo : public Object {
 public:
@@ -30,9 +31,10 @@ public:
         return this_node_;
     }
 
-    void server_init(size_t idx, size_t port, size_t num_nodes) {
+    void server_init(size_t idx, char* ip, size_t port, size_t num_nodes) {
         this_node_ = idx;
-        init_sock_(port);
+        init_sock_(ip, port);
+        std::cout << "initialized socket, listening on " << ip << ":" << port << "\n";
         num_nodes_ = num_nodes;
         nodes_ = new NodeInfo[num_nodes];
         for (size_t i = 0; i < num_nodes_; i++) nodes_[i].id = 0;
@@ -40,12 +42,13 @@ public:
         nodes_[0].id = 0;
         for (size_t i = 1; i < num_nodes; i++) {
             Register* msg = dynamic_cast<Register*>(recv_msg());
+            std::cout  << "Registration msg received, " << "client: " << msg->client << ":" << msg->port_ <<"\n";
             nodes_[msg->sender()].id = msg->sender();
             nodes_[msg->sender()].address.sin_family = AF_INET;
             inet_pton(AF_INET, msg->client, &nodes_[msg->sender()].address.sin_addr);
-            nodes_[msg->sender()].address.sin_port = htons(msg->port);
+            nodes_[msg->sender()].address.sin_port = htons(msg->port_);
         }
-        size_t* ports = new size_t[num_nodes_];
+        size_t *ports = new size_t[num_nodes_];
         String** addresses = new String*[num_nodes_];
         for (size_t i = 0; i < num_nodes_; i++) {
             ports[i] = ntohs(nodes_[i].address.sin_port);
@@ -53,7 +56,7 @@ public:
             inet_ntop(AF_INET, &nodes_[i].address.sin_addr, client_ip, INET_ADDRSTRLEN);
             addresses[i] = new String(client_ip);
         }
-        Directory ipd(this_node_, -1, num_nodes_, ports, addresses);
+        Directory ipd(this_node_, 0, num_nodes_, ports, addresses);
 
         for (size_t i = 1; i < num_nodes_; i++) {
             ipd.target_ = i;
@@ -61,51 +64,73 @@ public:
         }
     }
 
-    void client_init(size_t idx, size_t port, char* server_adr, size_t server_port, size_t num_nodes) {
+    void client_init(size_t idx, char* ip, size_t port, char* server_adr, size_t server_port) {
         this_node_ = idx;
-        num_nodes_ = num_nodes;
-        init_sock_(port);
+        init_sock_(ip, port);
         nodes_ = new NodeInfo[1];
         nodes_[0].id = 0;
         nodes_[0].address.sin_family = AF_INET;
+        nodes_[0].address.sin_addr.s_addr = inet_addr(ip);
         nodes_[0].address.sin_port = htons(server_port);
-        assert((inet_pton(AF_INET, server_adr, &nodes_[0].address.sin_addr) > 0));
+        
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &ip_.sin_addr, client_ip, INET_ADDRSTRLEN);
+        std::cout << "creating registration msg, port: " << port <<"\n";
         Register reg(idx, client_ip, port);
 
         send_msg(&reg);
 
         Directory* ipd = dynamic_cast<Directory*>(recv_msg());
-        NodeInfo * nodes = new NodeInfo[num_nodes];
-        for (size_t i = 0; i < ipd->clients; i++) {
+        num_nodes_ = ipd->clients;
+        NodeInfo * nodes = new NodeInfo[num_nodes_];
+        for (size_t i = 0; i < num_nodes_; i++) {
+            char* node_ip = ipd->addresses[i]->c_str();
+            size_t node_port = ipd->ports[i];
+            std::cout << "Directory: node " << i << ", ip: " <<  node_ip << ", port: " << node_port << "\n";
             nodes[i].id = i;
             nodes[i].address.sin_family = AF_INET;
-            nodes[i].address.sin_port = htons(ipd->ports[i]);
-            assert((inet_pton(AF_INET, ipd->addresses[i]->c_str(), &nodes[i].address.sin_addr)) > 0);
+            nodes[i].address.sin_port = htons(node_port);
+            nodes[i].address.sin_addr.s_addr = inet_addr(node_ip);
         }
         delete[] nodes;
         nodes_ = nodes;
         delete ipd;
     }
 
-    void init_sock_(size_t port) {
-        assert((sock_ = socket(AF_INET, SOCK_STREAM, 0) >= 0));
+    void init_sock_(char* ip, size_t port) {
+        assert((sock_ = socket(AF_INET, SOCK_STREAM, 0)) >= 0);
         int opt =1;
         assert(setsockopt(sock_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt) == 0));
         ip_.sin_family = AF_INET;
-        ip_.sin_addr.s_addr = INADDR_ANY;
+        ip_.sin_addr.s_addr = inet_addr(ip);
+        //inet_pton(AF_INET, ip, &ip_.sin_addr);
         ip_.sin_port = htons(port);
-        assert(bind(sock_, (sockaddr*) &ip_, sizeof(ip_)) >= 0);
-        assert((listen(sock_, 100) >= 0)); // connection queue size up to 100
+        //int res = bind(sock_, (sockaddr*) &ip_, sizeof(ip_));
+        if( bind(sock_, (sockaddr*) &ip_, sizeof(ip_)) < 0){
+            perror("bind failed. Error");
+            return;
+        }
+        if( (listen(sock_, 100)) < 0){
+            perror("listen failed. Error");
+            return;
+        }
     }
 
     void send_msg(Message* msg) {
         NodeInfo &target = nodes_[msg->target()];
+        int server_port = ntohs(target.address.sin_port);
+        char server_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &target.address.sin_addr, server_ip, INET_ADDRSTRLEN);
+        std::cout << "attempting to send msg to: " << server_ip << ":" << server_port << "\n";
         int connection = socket(AF_INET, SOCK_STREAM, 0);
-        assert(connect >= 0);
-        assert(connect(connection, (sockaddr*)&target.address, sizeof(target.address)) >= 0);
+        assert(connection >= 0);
+        if (connect(connection, (sockaddr*)&target.address, sizeof(target.address)) < 0) {
+            perror("connection failed. Error");
+            return;
+        }
+        //assert(connect(connection, (sockaddr*)&target.address, sizeof(target.address)) >= 0);
         char* serialized = msg->serialize();
+        std::cout << "sending: " << serialized << "\n";
         size_t length = strlen(serialized);
         send(connection, &length, sizeof(size_t), 0);
         send(connection, serialized, length, 0);
@@ -122,7 +147,9 @@ public:
         while (rd != size) {
             rd+= read(req, buf + rd, size - rd);
         }
-        Message* msg = new Message(buf);
+        std::cout << buf << "\n";
+        Deserializable ds;
+        Message* msg = dynamic_cast<Message*>(ds.deserialize(buf));
         return msg;
     }
 };
