0e385c07f840c433957703c544b4426907a7b865
Yash J Shetty <yash17shetty@gmail.com>
2020-04-09 21:45:37 -0400

    wip

--- src/application/application.h
+++ src/application/application.h
@@ -1,6 +1,7 @@
 //lang::CwC
 #pragma once
 #include "../object.h"
+#include "../store/key.h"
 #include "../store/kvstore.h"
 #include "../dataframe/distributedDataframe.h"
 #include "../serialize/deserialize.h"
@@ -16,7 +17,7 @@ public:
     //Networked Application Constructor
     Application(size_t idx, NetworkIP* network) {
         this->this_node_ = idx;
-        this->kv_ = new KVStore(1, idx, network);
+        this->kv_ = new KVStore(network->num_nodes_, idx, network);
     }
     //Legacy single-node constructor. Currently used in WordCount app, hoping to phase out
     Application(size_t idx) {
@@ -26,6 +27,9 @@ public:
     //Virtual run method that begins all Applications
     virtual void run_() {}
 
+    size_t this_node() {
+      kv_->num_nodes_;
+    }
 };
 
 //Trivial use case of the application class using networking
@@ -45,5 +49,46 @@ class Trivial : public Application {
     for (size_t i = 0; i < SZ; ++i) sum -= df2->get_double(0,i);
     assert(sum==0);
     delete df; delete df2;
+  }
+};
+
+
+class Demo : public Application {
+public:
+  Key* main =  new Key("main", 0);
+  Key* verify = new Key("verif",0);
+  Key* check = new Key("ck",0);
+ 
+  Demo(size_t idx): Application(idx) {}
+ 
+  void run_() override {
+    switch(this_node()) {
+    case 0:   producer();     break;
+    case 1:   counter();      break;
+    case 2:   summarizer();
+   }
+  }
+ 
+  void producer() {
+    size_t SZ = 100*1000;
+    double* vals = new double[SZ];
+    double sum = 0;
+    for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
+    DistributedDataFrame::fromArray(main, kv_, SZ, vals);
+    DataFrame::fromScalar(&check, &kv, sum);
+  }
+ 
+  void counter() {
+    DistributedDataFrame* v = dynamic_cast<DistributedDataFrame*>(kv_->waitAndget(main));
+    size_t sum = 0;
+    for (size_t i = 0; i < 100*1000; ++i) sum += v->get_double(0,i);
+    p("The sum is  ").pln(sum);
+    DataFrame::fromScalar(&verify, kv_, sum);
+  }
+ 
+  void summarizer() {
+    DistributedDataFrame* result = dynamic_cast<DistributedDataFrame*>(kv_->waitAndget(verify));
+    DistributedDataFrame* expected = dynamic_cast<DistributedDataFrame*>(kv_->waitAndget(check));
+    pln(expected->get_double(0,0)==result->get_double(0,0) ? "SUCCESS":"FAILURE");
   }
 };
